41a4028d089cea67243c9110f3d3f3e9524f90cc
Ankitkumar147 <aankitchouhan3@gmail.com>
2022-08-28 17:10:11 +0530

    Added Extend Service layer to the Greeting controller.

--- GreetingApp/src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ GreetingApp/src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -1,25 +1,26 @@
 package com.bridgelabz.greetingapp.controller;
 
-import com.bridgelabz.greetingapp.Model.Greeting;
+import com.bridgelabz.greetingapp.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class GreetingController {
-    public static final String template = "Hello , %s!";
-    private final AtomicLong counter = new AtomicLong();
 
-    /** UC1- Created Simple Spring App that return message to the user.
-     *
-     * @param name
+    @Autowired
+    private GreetingService greetingService;
+
+    /** UC1- Created Simple Spring App that return message to
+     * the user by using Service layer.
+     * @param
      * @return test Using Curl
      */
-    @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "world")String name){
-        return new Greeting(counter.incrementAndGet(),
-        String.format(template, name));
+    @GetMapping(value = "/getMessage")
+    public ResponseEntity<String> getMessage() {
+        return new ResponseEntity<>(greetingService.getMessage(), HttpStatus.OK);
     }
+
 }
